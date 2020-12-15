bf38f6b0db3d173fbfca412bd453c456e0900243
Nash <72932477+Nashwalt@users.noreply.github.com>
2020-12-15 17:44:01 -0500

    Added event listener for youtube button.

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    //event listener for yt button
+    $("#youtube").click(function(e){
+    e.preventDefault();
+    $("#videos").empty();  // clears videos when submit button clicked
+
+})
+})
